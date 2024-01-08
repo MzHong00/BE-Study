35ff4315b0b2ece26bf451698fefb9e4ec909994
unknown <rkawk1539@gmail.com>
2024-01-08 15:20:11 +0900

    added comment

--- db-study/mysql/index.js
+++ db-study/mysql/index.js
@@ -1,9 +1,12 @@
 var mysql = require('mysql');
+
+//mysql 세팅이 되어야 아래 코드들도 실행이 성공적임
+//일반적인 mysql 커넥션 생성과 쿼리문 실행 함수 및 종료
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'my_db'
+    database: 'my_db' //schema 이름
 });
 
 connection.connect();
@@ -15,7 +18,8 @@ connection.query('SELECT * from user', function (error, results, fields) {
 
 connection.end();
 
-// 모듈화한 커넥션 풀 사용
+
+// conPoolModuel.js에서 모듈화한 커넥션 풀 사용
 // pool((mysql) => {
 //     mysql.query("select * from user", function (error, results, fields) {
 //         if (error) throw error;
